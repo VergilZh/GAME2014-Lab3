422df82cecbc88c65fbf81088f6f1a1cc40cea9c
VergilZh <z1205067147@gmail.com>
2020-10-09 15:10:03 -0400

    Initial commit

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public float verticalSpeed;
+    public float verticalBoundary;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();    
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        transform.position -= new Vector3(0.0f, verticalSpeed);
+    }
+
+    private void _Reset()
+    {
+        transform.position = new Vector3(0.0f,verticalBoundary);
+    }
+
+    private void _CheckBounds()
+    {
+        if(transform.position.y <= -verticalBoundary)
+        {
+            _Reset();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalBoundary;
+    public float horizontalSpeed;
+    public float maxSpeed;
+    private Rigidbody2D m_rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+    private void _Move()
+    {
+        var direction = 0.0f;
+
+        foreach(var touch in Input.touches)
+        {
+            var worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
+            
+            if(worldTouch.x>transform.position.x)
+            {
+                direction = 1.0f;
+            }
+
+            if(worldTouch.x < transform.position.x)
+            {
+                direction = -1.0f;
+            }
+
+        }
+
+
+        if ((Input.GetAxis("Horizontal") >= 0.1f))
+        {
+            direction = 1.0f;
+        }
+
+        if ((Input.GetAxis("Horizontal") <= -0.1f))
+        {
+            direction = -1.0f;
+        }
+
+        Vector2 newVelocity = m_rigidBody.velocity + new Vector2(direction * horizontalSpeed, 0.0f);
+        m_rigidBody.velocity = Vector2.ClampMagnitude(newVelocity, maxSpeed);
+        m_rigidBody.velocity *= 0.99f;
+    }
+
+    private void _CheckBounds()
+    {
+       if(transform.position.x >= horizontalBoundary)
+        {
+            transform.position = new Vector3(horizontalBoundary, transform.position.y, 0.0f);
+        }
+
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            transform.position = new Vector3(-horizontalBoundary, transform.position.y, 0.0f);
+        }
+    }
+}
